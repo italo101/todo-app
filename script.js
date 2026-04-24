60c65af7a98634ce63b95d2c99cf73e5e6acff80
italo101 <143236700+italo101@users.noreply.github.com>
2026-04-24 01:47:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
 
+  if (taskText === "") return;
+
+  let li = document.createElement("li");
+  li.textContent = taskText;
+
+  // Click to remove task
+  li.onclick = function () {
+    li.remove();
+  };
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
